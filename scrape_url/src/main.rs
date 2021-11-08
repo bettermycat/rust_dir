59ffaa2e9c3d04d5122b39ec0ba94f9e8125dcf1
hufan <hufan@qding.me>
2021-11-08 11:48:22 +0800

    add o

---
+++ scrape_url/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+
+    let url = "https://www.rust-lang.org";
+    let output = "rust.md";
+
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+
+    fs::write(output, md.as_bytes()).unwrap();
+    print!("Converted markdown has been saved in {}.", output);
+}
